94112c06e2fccc16cc126f7fe0e41972848a629e
Lukas Haidacher <lukas.haidacher@innio.com>
2020-06-20 12:34:08 +0200

    adjusted to meta config requirments

---
+++ app/src/main/java/at/fhj/swd/travx/TrevXApplication.java
@@ -0,0 +1,10 @@
+package at.fhj.swd.travx;
+
+import android.app.Application;
+
+public class TrevXApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
