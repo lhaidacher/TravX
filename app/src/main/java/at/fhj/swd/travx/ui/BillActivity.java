df596385018612e220a89044da82f5ec9e17599d
Lukas Haidacher <lukas.haidacher@innio.com>
2020-06-21 00:07:57 +0200

    added bill map

--- app/src/main/java/at/fhj/swd/travx/ui/BillActivity.java
+++ app/src/main/java/at/fhj/swd/travx/ui/BillActivity.java
@@ -1,6 +1,8 @@
 package at.fhj.swd.travx.ui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -9,6 +11,14 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.Objects;
 
@@ -20,14 +30,18 @@ import at.fhj.swd.travx.util.DateUtils;
 import at.fhj.swd.travx.util.StringUtils;
 import at.fhj.swd.travx.util.ThreadUtils;
 
-public class BillActivity extends AppCompatActivity {
+public class BillActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener {
     private Long billId;
+    private Bill bill;
     private String journeyName;
 
     private TextView tvValue;
     private TextView tvLatitude;
     private TextView tvLongitude;
     private TextView tvCreated;
+
+    private MapView mapView;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +60,10 @@ public class BillActivity extends AppCompatActivity {
         tvLongitude = findViewById(R.id.tvBillLon);
         tvCreated = findViewById(R.id.tvBillCreated);
 
+        mapView = findViewById(R.id.billMap);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         Toolbar topAppBar = findViewById(R.id.topAppBar);
 
         topAppBar.setNavigationOnClickListener(view -> {
@@ -58,7 +76,7 @@ public class BillActivity extends AppCompatActivity {
     }
 
     private void setContent() {
-        Bill bill = Database.getInstance(this)
+        bill = Database.getInstance(this)
                 .billDao()
                 .findById(billId);
 
@@ -71,5 +89,59 @@ public class BillActivity extends AppCompatActivity {
             tvLongitude.setText(StringUtils.format(bill.getLongitude()));
             tvCreated.setText(DateUtils.formatExtended(bill.getCreatedAt()));
         });
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setOnInfoWindowClickListener(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+            map.setMyLocationEnabled(true);
+
+        map.clear();
+        MarkerOptions markerOptions = new MarkerOptions()
+                .title("Bill")
+                .position(new LatLng(bill.getLatitude(), bill.getLongitude()));
+        map.addMarker(markerOptions);
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+        Log.i("LIFECYCLE", "onPause map");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+        Log.i("LIFECYCLE", "onStop map");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+        Log.i("LIFECYCLE", "onDestroy map");
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
